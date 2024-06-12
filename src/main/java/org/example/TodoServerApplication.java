f945cbc676d591aee2718fab2bb00546d6029ec4
HEE NAMKUNG <hee@HEEs-MacBook-Pro-2.local>
2024-06-12 17:42:40 +0900

    Update TODO4

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class TodoServerApplication {
     public static void main(String[] args){
-        System.out.println("Hello TODO3");
+        System.out.println("Hello TODO4");
     }
 }
