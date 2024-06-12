baf862b89ef4d77e50c10d76d4c7650a675c61ac
HEE NAMKUNG <hee@HEEs-MacBook-Pro-2.local>
2024-06-12 18:02:23 +0900

    TODO8

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class TodoServerApplication {
     public static void main(String[] args){
-        System.out.println("Hello TODO6");
+        System.out.println("Hello TODO8");
     }
 }
