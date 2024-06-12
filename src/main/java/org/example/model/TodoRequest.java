2a03eb75331c951e949f7ba2286a4736ce1d57e5
unknown <bnamkung@gmail.com>
2024-06-12 13:09:42 +0900

    Initial commit

---
+++ src/main/java/org/example/model/TodoRequest.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoRequest {
+    private String title;
+    private long order;
+    private Boolean completed;
+}
